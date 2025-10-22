27f8c7f7aa5485a952c250c2f599b26ac2db72d4
bece21-mkenani <mphatsokenani0@gmail.com>
2025-10-22 10:16:18 +0000

    models added

---
+++ server/controllers/authController.js
@@ -0,0 +1,85 @@
+const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
+
+// @desc    Register a new user
+// @route   POST /api/auth/register
+// @access  Public
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Check if user exists
+    const userExists = await User.findOne({ 
+      $or: [{ email }, { username }] 
+    });
+
+    if (userExists) {
+      return res.status(400).json({ 
+        message: 'User already exists with this email or username' 
+      });
+    }
+
+    // Create user
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        profilePicture: user.profilePicture,
+        token: generateToken(user._id),
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc    Login user
+// @route   POST /api/auth/login
+// @access  Public
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    if (user && (await user.comparePassword(password))) {
+      res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        profilePicture: user.profilePicture,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc    Get current user
+// @route   GET /api/auth/me
+// @access  Private
+const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
--- server/models/User.js
+++ server/models/User.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
     default: ''
   }
 }, {
-  timestamps: true 
+  timestamps: true
 });
 
 // Hash password before saving
